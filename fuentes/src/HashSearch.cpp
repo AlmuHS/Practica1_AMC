5ba583cee974320cbedf77007c0843bbce95b5ed
AlmuHS <liberamenso10000@gmail.com>
2017-11-12 13:16:22 +0100

    Mejorada tabla hash cerrada

--- fuentes/src/HashSearch.cpp
+++ fuentes/src/HashSearch.cpp
@@ -17,46 +17,38 @@ void HashSearch::add_element_opened(vector<int> v)
 
     for(int i = 0; i < v.size(); i++)
     {
-        int pos = v[i] % v.size(); //Funcion hash
+        int pos = v[i] % v.size();
+
         hash_opened[pos].push_back(v[i]);
     }
-}
-
-
-int HashSearch::closed_hash(int intempts, int key, int size){
-    if (intempts < 1) return (1 + key % (size - 2) );
-    else
-        return (closed_hash(intempts - 1, key, size) + closed_hash(0, key, size)) % size;
 }
 
 void HashSearch::add_element_closed(vector<int> v)
 {
     hash_closed.assign(v.size(), -1);
     int pos = 0;
-    int max_intentos = v.size()/2;
+
+    bool vacia = false;
+    int intentos = 1;
 
     for(int i = 0; i < v.size(); i++)
     {
-        bool vacia = false;
-        int intentos = 1;
+        vacia = false;
+        intentos = 1;
 
-        //Si la posicion esta ocupada, se recalcula la hash a partir del numero de intentos realizados
-        while(intentos < max_intentos && !vacia){
-            pos = trunc(v.size() * (v[i]* inv_aurea * intentos - trunc(v[i] * inv_aurea * intentos) ) );
-            //pos = closed_hash(intentos, v[i], v.size());
+        while(!vacia){
+
+            if(intentos < v.size()/2)
+                pos = trunc(v.size() * (v[i]* inv_aurea * intentos - trunc(v[i] * inv_aurea * intentos) ) );
+
+            else pos = (v[i] + intentos) % v.size();
 
             if(hash_closed[pos] == -1) vacia = true;
-            else intentos++;
-        }//Fin while
+            intentos++;
+        }
 
         if(vacia) hash_closed[pos] = v[i];
 
-        else if(intentos == max_intentos){
-            pos = 0;
-            while(hash_closed[pos] != -1) pos++;
-
-            if(pos < hash_closed.size()) hash_closed[pos] = v[i];
-        }
     }
 }
 
@@ -78,23 +70,20 @@ int HashSearch::search_element_opened(int key)
 
 int HashSearch::search_element_closed(int key)
 {
-
-    int pos = key % hash_closed.size();
     int intentos = 1;
-    int max_intentos = hash_closed.size()/2;
 
-    while(hash_closed[pos] != key && intentos < max_intentos){
-        pos = trunc(hash_closed.size() * (key * inv_aurea * intentos - trunc(key * inv_aurea * intentos) ) );
-        //pos = closed_hash(intentos, key, hash_closed.size());
+    int pos = trunc(hash_closed.size() * (key * inv_aurea - trunc(key * inv_aurea) ));
+
+    while(hash_closed[pos] != key){
+
+        if(intentos < hash_closed.size()/2)
+            pos = trunc(hash_closed.size() * (key * inv_aurea * intentos - trunc(key * inv_aurea * intentos) ) );
+
+        else pos = (key + intentos) % hash_closed.size();
         intentos++;
     }
+
     if(hash_closed[pos] == key) return pos;
-
-    else if(intentos == max_intentos){
-        std::vector<int>::iterator it = std::find(begin(hash_closed), end(hash_closed), key);
-
-        if(it != hash_closed.end()) return pos;
-    }
 
     return -1;
 }
