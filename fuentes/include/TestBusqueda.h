13683b67f2fd9cca0c36461dcdd54e2887cc852d
AlmuHS <liberamenso10000@gmail.com>
2017-11-19 22:23:00 +0100

    Añadida cabecera GPL

--- fuentes/include/TestBusqueda.h
+++ fuentes/include/TestBusqueda.h
@@ -1,3 +1,21 @@
+/*Copyright 2013 2014 2016 2017 Almudena Garcia Jurado-Centurion
+
+This file is part of Practica1_AMC.
+
+Practica1_AMC is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Practica1_AMC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Practica1_AMC.  If not, see <http://www.gnu.org/licenses/>.*/
+
+
 #include <vector>
 #include "AlgoritmosBusqueda.h"
 #include "Mtime.h"
@@ -11,8 +29,6 @@
 #include <chrono>
 #include <string>
 
-//using namespace std;
-
 static const int BINARIA = 0;
 static const int SECUENCIAL = 1;
 static const int HASHCLOSED = 2;
@@ -21,7 +37,7 @@ static const int HASHOPENED = 3;
 
 class TestBusqueda
 {
-	vector<string> nombreAlgoritmo;
+	std::vector<string> nombreAlgoritmo;
 public:
 	TestBusqueda(void);
 	~TestBusqueda(void);
@@ -29,7 +45,7 @@ public:
 	void comparar(int metodo1, int metodo2);
 	void casoMedio(int metodo);
 	double Buscar(std::vector<int> &v, int metodo, int key);
-	void generar_grafica(string metodo);
-	void generar_grafica(string fichero1, string fichero2);
+	void generar_grafica(std::string metodo);
+	void generar_grafica(std::string fichero1, std::string fichero2);
 };
 
